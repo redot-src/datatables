cc23df44610f1d2feee856e769d70d92f2106b9e
AbdelrhmanSaid <abdelrhmansaidzaki@hotmail.com>
2024-06-18 18:13:01 +0300

    refactor: Update DatatablesServiceProvider to improve configuration and view registration

---
+++ config/datatables.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Redot Datatables config
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify the configuration of the livewire-datatable.
+    |
+    */
+
+    // ...
+];
--- src/DatatablesServiceProvider.php
+++ src/DatatablesServiceProvider.php
@@ -11,7 +11,38 @@ class DatatablesServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // ...
+        $this->config();
+        $this->views();
+    }
+
+    /**
+     * Register the package configuration.
+     */
+    protected function config(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/datatables.php',
+            'datatables'
+        );
+
+        $this->publishes([
+            __DIR__.'/../config/datatables.php' => config_path('datatables.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the package views.
+     */
+    protected function views(): void
+    {
+        $this->loadViewsFrom(
+            __DIR__.'/../resources/views',
+            'datatables'
+        );
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/datatables'),
+        ], 'views');
     }
 
     /**
