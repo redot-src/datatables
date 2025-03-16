a8fe697924b930b736c7ef6823b564c946e1cf1d
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-03-17 01:27:52 +0200

    hide thead if there's no rows

--- resources/views/partials/table.blade.php
+++ resources/views/partials/table.blade.php
@@ -3,38 +3,40 @@
         'table card-table table-vcenter datatable',
         'bordered' => $bordered,
     ])>
-        <thead @class(['sticky-top' => $stickyHeader])>
-            <tr>
-                @foreach ($columns as $column)
-                    <th @class(['fixed-' . $column->fixedDirection => $column->fixed])>
-                        @if ($column->sortable && $column->name)
-                            <a href="#" class="text-decoration-none text-muted"
-                                wire:click.prevent="sort('{{ $column->name }}')">
-                                <span class="me-1">
-                                    {{ $column->label }}
-                                </span>
+        @if ($rows->isEmpty() === false)
+            <thead @class(['sticky-top' => $stickyHeader])>
+                <tr>
+                    @foreach ($columns as $column)
+                        <th @class(['fixed-' . $column->fixedDirection => $column->fixed])>
+                            @if ($column->sortable && $column->name)
+                                <a href="#" class="text-decoration-none text-muted"
+                                    wire:click.prevent="sort('{{ $column->name }}')">
+                                    <span class="me-1">
+                                        {{ $column->label }}
+                                    </span>
 
-                                @if ($sortColumn === $column->name)
-                                    @if ($sortDirection === 'asc')
-                                        <i class="fas fa-sort-up"></i>
+                                    @if ($sortColumn === $column->name)
+                                        @if ($sortDirection === 'asc')
+                                            <i class="fas fa-sort-up"></i>
+                                        @else
+                                            <i class="fas fa-sort-down"></i>
+                                        @endif
                                     @else
-                                        <i class="fas fa-sort-down"></i>
+                                        <i class="fas fa-sort"></i>
                                     @endif
-                                @else
-                                    <i class="fas fa-sort"></i>
-                                @endif
-                            </a>
-                        @else
-                            {{ $column->label }}
-                        @endif
-                    </th>
-                @endforeach
+                                </a>
+                            @else
+                                {{ $column->label }}
+                            @endif
+                        </th>
+                    @endforeach
 
-                @if ($actions)
-                    <th class="w-1 fixed-end datatable-actions"></th>
-                @endif
-            </tr>
-        </thead>
+                    @if ($actions)
+                        <th class="w-1 fixed-end datatable-actions"></th>
+                    @endif
+                </tr>
+            </thead>
+        @endif
 
         <tbody wire:loading.class="opacity-50">
             @forelse($rows as $row)
