1daf1c02fea8b057210e50505bc2d253001ccffd
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-09-10 13:30:38 +0300

    create a new custom filter

---
+++ src/Filters/CustomFilter.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Redot\Datatables\Filters;
+
+use Closure;
+use Illuminate\Database\Eloquent\Builder;
+use InvalidArgumentException;
+
+class CustomFilter extends Filter
+{
+    /**
+     * The base to apply the filter to the query.
+     */
+    public string $base;
+
+    /**
+     * The callback to apply the filter to the query.
+     */
+    public Closure $callback;
+
+    /**
+     * Set the filter base.
+     */
+    public function base(string $base): self
+    {
+        if (! class_exists($base) || ! is_subclass_of($base, Filter::class)) {
+            throw new InvalidArgumentException('The base must be a valid filter class.');
+        }
+
+        $this->base = $base;
+
+        return $this;
+    }
+
+    /**
+     * Set the callback to apply the filter to the query.
+     */
+    public function callback(Closure $callback): self
+    {
+        $this->callback = $callback;
+
+        return $this;
+    }
+
+    /**
+     * Render the filter view.
+     */
+    public function render(): \Illuminate\Contracts\View\View
+    {
+        $base = new $this->base;
+
+        return view($base->view, ['filter' => $this]);
+    }
+
+    /**
+     * Apply the filter to the given query.
+     */
+    public function apply(Builder $query, $value): void
+    {
+        // Early return if the callback is not set.
+        if (! $this->callback) {
+            return;
+        }
+
+        // Apply the filter to the query.
+        $this->withRelation($this->column, $query, function (Builder $query) use ($value) {
+            call_user_func($this->callback, $query, $value);
+        });
+    }
+}
